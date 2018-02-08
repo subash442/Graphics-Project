e997d288be90c203b5b87343c983c20f67538de9
Subash Timilsina <072bex442.subash@pcampus.edu.np>
2018-02-08 12:49:43 +0545

    Point plotted

---
+++ Colors.h
@@ -0,0 +1,10 @@
+#ifndef COLORS_H_INCLUDED
+#define COLORS_H_INCLUDED
+
+#define BLACK        0, 0, 0, 0
+#define WHITE        255,255,255,255
+#define RED          255, 0, 0, 255
+
+
+
+#endif // COLORS_H_INCLUDED
